c6b32955f4c0aac3418acb43655f85dedc42da8a
Tzur1234 <tzur09@gmail.com>
2023-04-16 19:08:07 +0300

    add an item

---
+++ shopping_list/script.js
@@ -0,0 +1,47 @@
+
+const inputItem = document.getElementById('item-input') 
+const itemForm = document.getElementById('item-form')  
+const itemList = document.getElementById('item-list')
+
+function createButton(classes){
+    button = document.createElement('button')
+    button.className = classes
+    
+    // create icon 
+    icon = document.createElement('i')
+    icon.className = 'fa-solid fa-xmark'
+
+    // add the icon to the button 
+    button.appendChild(icon)
+
+    return button
+
+}
+
+function addItem(e){
+    e.preventDefault()
+
+    // validation
+    if (inputItem.value === ''){
+        console.log('Please add item');
+        return;
+    }
+
+    // Add an item
+    const li = document.createElement('li')
+
+    // add the item name
+    li.appendChild(document.createTextNode(inputItem.value))
+    
+    // add a button 
+    button = createButton('remove-item btn-link text-red')
+    li.appendChild(button)
+
+
+    // append the item to the list
+    itemList.appendChild(li)
+
+    
+}
+
+itemForm.addEventListener('submit', addItem);   
