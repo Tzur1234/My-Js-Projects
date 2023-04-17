6eb368d8748216a082d52116348698f12a5deb6e
Tzur1234 <tzur09@gmail.com>
2023-04-17 23:47:33 +0300

    add random_user_generator project

---
+++ random_user_generator/script.js
@@ -0,0 +1,66 @@
+
+function fetchUser(){
+  showSpinner()
+  fetch('https://randomuser.me/api/')
+  .then((res) => res.json())
+  .then((data) => {
+    hideSpinner()
+    displayUser(data.results[0])
+  });
+}
+
+
+function displayUser(user){
+  if(user.gender === 'male'){
+    document.body.style.backgroundColor = '#4C62FF'
+  }
+  else{
+    document.body.style.backgroundColor = '#E52967' ; 
+  }
+
+  // insert data
+  details = document.getElementById('user')
+
+  console.log(user)
+
+  details.innerHTML = `
+                      <div class="flex justify-between">
+                      <div class="flex">
+                        <img
+                          class="w-48 h-48 rounded-full mr-8"
+                          src="${user.picture.large}"
+                        />
+                        <div class="space-y-3">
+                          <p class="text-xl">
+                            <span class="font-bold">Name: </span>
+                            ${user.name.first} ${user.name.last}
+                          </p>
+                          <p class="text-xl">
+                            <span class="font-bold">Email: </span> ${user.email}
+                          </p>
+                          <p class="text-xl">
+                            <span class="font-bold">Phone: </span> ${user.phone}
+                          </p>
+                          <p class="text-xl">
+                            <span class="font-bold">Location: </span> ${user.location.city} ${user.location.country}
+                          </p>
+                          <p class="text-xl"><span class="font-bold">Age: </span> ${user.dob.age}</p>
+                        </div>
+                      </div>
+                    </div>`
+
+                   
+}
+
+function showSpinner(){
+  document.querySelector('.spinner').style.display = 'block'
+
+}
+
+function hideSpinner(){
+  document.querySelector('.spinner').style.display = 'none'
+
+}
+
+button = document.getElementById('generate').addEventListener('click', fetchUser)
+
