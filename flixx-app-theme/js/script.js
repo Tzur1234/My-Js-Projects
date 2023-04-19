9f6cb24fde450191ce23268a98fe1ef8a3c1174d
Tzur1234 <tzur09@gmail.com>
2023-04-19 18:32:32 +0300

    add movie details file

--- flixx-app-theme/js/script.js
+++ flixx-app-theme/js/script.js
@@ -89,19 +89,46 @@ async function displayPopularTvShows() {
     }); 
 }
 
+// Display Backdrop On Details Pages
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+      } else {
+        document.querySelector('#show-details').appendChild(overlayDiv);
+      }
+
+}
+
 async function displayMovieDetail() {
 
     // fetch the movie id from the url
     const queryString = window.location.search
     
     const urlParams = new URLSearchParams(queryString)
-
+    
     movie_id = urlParams.get('id')
-
-
+    
+    
     movie_data = await fetchData(`/movie/${movie_id}`)
+    
+    
+    // Overlay for background image
+    displayBackgroundImage('movie', movie_data.backdrop_path);
 
-    console.log(movie_data)
+
 
     document.getElementById('movie-details').innerHTML = `
     <div class="details-top">
@@ -128,9 +155,6 @@ async function displayMovieDetail() {
       <h5>Genres</h5>
       <ul class="list-group">
       ${ movie_data.genres.map((genre) => `<li> ${genre.name} </li>`).join(' ')}
-        <li>Genre 1</li>
-        <li>Genre 2</li>
-        <li>Genre 3</li>
       </ul>
       <a href="${movie_data.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
     </div>
@@ -138,9 +162,9 @@ async function displayMovieDetail() {
   <div class="details-bottom">
     <h2>Movie Info</h2>
     <ul>
-      <li><span class="text-secondary">Budget:</span> $${movie_data.budget}</li>
-      <li><span class="text-secondary">Revenue:</span> $${movie_data.revenue}</li>
-      <li><span class="text-secondary">Runtime:</span> ${movie_data.runtime}</li>
+      <li><span class="text-secondary">Budget:</span> $ ${movie_data.budget.toLocaleString("en-US")}</li>
+      <li><span class="text-secondary">Revenue:</span> $ ${movie_data.revenue.toLocaleString("en-US")}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie_data.runtime} minutes</li>
       <li><span class="text-secondary">Status:</span> ${movie_data.status}</li>
     </ul>
     <h4>Production Companies</h4>
@@ -149,13 +173,66 @@ async function displayMovieDetail() {
     </div>
   </div>
     `
+}
 
-    // append compenies
-    compenies = document.querySelectorAll('list-group')[1]   
-    movie_data.production_companies.forEach(comp => {
-        console.log(comp.name)
-        compenies.appendChild(document.createTextNode(`${comp.name},`))
-    })
+// show details
+async function displayShowDetails(){
+
+    // fetch show id
+    const queryString = window.location.search
+    
+    const urlParams = new URLSearchParams(queryString)
+    
+    show_id = urlParams.get('id')
+
+    show_data = await fetchData(`/tv/${show_id}`)
+
+    console.log(show_data)
+
+
+    document.querySelector('#show-details').innerHTML = `
+    <div class="details-top">
+    <div>
+      <img
+      src="${
+            show_data.backdrop_path ? `https://image.tmdb.org/t/p/w500${show_data.backdrop_path}`
+        : `./flixx-app-theme/images/no-image.jpg` 
+            }"
+        class="card-img-top"
+        alt="${show_data.name}"
+      />
+    </div>
+    <div>
+      <h2>${show_data.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${Math.round(show_data.vote_average)} / 10
+      </p>
+      <p class="text-muted">Release Date:  ${show_data.first_air_date}</p>
+      <p>
+      ${show_data.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+      ${ show_data.genres.map((genre) => `<li> ${genre.name} </li>`).join(' ')}
+      </ul>
+      <a href="${show_data.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number Of Episodes:</span> ${show_data.number_of_episodes}</li>
+      <li>
+        <span class="text-secondary">Last Episode To Air: ${show_data.last_episode_to_air.name}</span> 
+   
+      </li>
+      <li><span class="text-secondary">Status:</span> ${show_data.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">C${ show_data.production_companies.map((comp) => `${comp.name},`).join(' ')}</div>
+  </div>
+    `
 
 }
 
@@ -201,6 +278,7 @@ function init(){
             console.log('movie-details');
             break;
         case '/flixx-app-theme/tv-details.html':
+            displayShowDetails()
             console.log('tv-details');
             break;
         case '/flixx-app-theme/search.html':
@@ -214,4 +292,3 @@ function init(){
 }
 
 init()
-s
