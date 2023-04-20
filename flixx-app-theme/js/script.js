e6782a69baadba007cb11e3ed6cad7b0d523c480
Tzur1234 <tzur09@gmail.com>
2023-04-20 12:47:06 +0300

    add search movie/tv-show fuctinality

--- flixx-app-theme/js/script.js
+++ flixx-app-theme/js/script.js
@@ -1,7 +1,18 @@
 
 
 const global = {
-    currentUrl : window.location.pathname
+    currentUrl : window.location.pathname,
+    search: {
+      term: '',
+      type: '',
+      page: 1,
+      totalPages: 1
+    },
+    api : {
+      key : '1bcda823c623f469eb606c40d700de42',
+      url_path: 'https://api.themoviedb.org/3'
+
+    }
 }
 
 // General fetch function form TMBD API
@@ -10,25 +21,112 @@ async function fetchData(url){
     showSpinner()
     
     // credenentials
-    API_KEY = '1bcda823c623f469eb606c40d700de42'
-    BASE_URL_PATH = 'https://api.themoviedb.org/3'
+    API_KEY = global.api.key
+    BASE_URL_PATH = global.api.url_path
 
     FINAL_URL = `${BASE_URL_PATH}/${url}?api_key=${API_KEY}&language=en-US&page=1`
 
     const res = await fetch(FINAL_URL);
     const data = await res.json()
-    
-    
+        
     hideSpinner()
     return data
 }
+
+// General fetch function form TMBD API
+async function fetchAPIData(){
+  // Spinner
+  showSpinner()
+  
+  // credenentials
+  API_KEY = global.api.key
+  BASE_URL_PATH = global.api.url_path
+
+  FINAL_URL = `${BASE_URL_PATH}/search/${global.search.type}?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${global.search.term}`
+
+
+
+  const res = await fetch(FINAL_URL);
+  const data = await res.json()
+  
+  
+  hideSpinner()
+  return data
+}
+
+// serarch movies/shows 
+async function search(){
+  const queryString = window.location.search
+    
+  const urlParams = new URLSearchParams(queryString)
+  
+  // set url parameters in the global scope veriabels
+  global.search.term = urlParams.get('search-term')
+  global.search.type = urlParams.get('type')
+
+
+  // validation for not empty
+  if(global.search.term !== '' && global.search.term !== null){
+    const {results, total_pages, page} = await fetchAPIData();
+
+    // insert in the DOM
+    displaySearchResults(results)
+
+  }
+  else{
+    showAlert('You must fill the search field', 'error')
+  }  
+
+  // clean the search bar
+  document.querySelector('#search-term').value = ''
+  
+  
+  movie_data = await fetchData(`/movie/${movie_id}`)
+}
+
+// Show movies/tv-shows results
+function displaySearchResults(results){
+
+  if(results.length === 0){
+    showAlert('No results', 'error')
+    return;
+  }
+
+  results.forEach(result => {
+    let card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = 
+      
+    `<a href="${global.search.type === 'movie' ? `movie-details.html?id=${result.id}` :`tv-details.html?id=${result.id}`}">
+          <img src="${
+                      result.poster_path ? `https://image.tmdb.org/t/p/w500${result.poster_path}`
+                      : `./images/no-image.jpg` 
+                    }"
+          }" class="card-img-top" alt="${global.search.type === 'movie' ? result.title : result.name}">
+        </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${result.release_date}</small>
+            </p>
+          </div>`
+ 
+    // append to DOM
+    document.getElementById('search-results').appendChild(card)
+  });
+
+
+
+
+}
+
 
 async function displayPopularMovies() {
     // fetch the data
     let movies = await fetchData('/movie/popular')
     movies = movies.results
-    console.log(movies)
-    // show the data
+   
+    // set the data in the DOM
     movies.forEach(movie => {
         // create div-card
         const div = document.createElement('div')
@@ -38,7 +136,7 @@ async function displayPopularMovies() {
         
           <a href="movie-details.html?id=${movie.id}">
             <img
-            src="${movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : `./flixx-app-theme/images/no-image.jpg`  }"
+            src="${movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : `./images/no-image.jpg`  }"
 
             class="card-img-top"
             alt="${movie.title}"
@@ -60,7 +158,7 @@ async function displayPopularTvShows() {
     // fetch the data
     let shows = await fetchData('/tv/popular')
     shows = shows.results
-    console.log(shows)
+
     // show the data
     shows.forEach(show => {
         // create div-card
@@ -72,7 +170,7 @@ async function displayPopularTvShows() {
         <img
           src="${
             show.poster_path ? `https://image.tmdb.org/t/p/w500${show.poster_path}`
-            : `flixx-app-theme/images/no-image.jpg`}"
+            : `./images/no-image.jpg`}"
           class="card-img-top"
           alt="Show Title"
         />
@@ -119,7 +217,7 @@ async function displayMovieDetail() {
     
     const urlParams = new URLSearchParams(queryString)
     
-    movie_id = urlParams.get('id')
+    let movie_id = urlParams.get('id')
     
     
     movie_data = await fetchData(`/movie/${movie_id}`)
@@ -136,7 +234,7 @@ async function displayMovieDetail() {
       <img
         src="${
             movie_data.backdrop_path ? `https://image.tmdb.org/t/p/w500${movie_data.backdrop_path}`
-            : `./flixx-app-theme/images/no-image.jpg` 
+            : `./images/no-image.jpg` 
         }"
         class="card-img-top"
         alt="${movie_data.title}"
@@ -185,18 +283,17 @@ async function displayShowDetails(){
     
     show_id = urlParams.get('id')
 
+    // fetch the data
     show_data = await fetchData(`/tv/${show_id}`)
 
-    console.log(show_data)
-
-
+    // add to the DOM
     document.querySelector('#show-details').innerHTML = `
     <div class="details-top">
     <div>
       <img
       src="${
             show_data.backdrop_path ? `https://image.tmdb.org/t/p/w500${show_data.backdrop_path}`
-        : `./flixx-app-theme/images/no-image.jpg` 
+        : `./images/no-image.jpg` 
             }"
         class="card-img-top"
         alt="${show_data.name}"
@@ -237,8 +334,6 @@ async function displayShowDetails(){
 }
 
 
-
-
 // Highlight active link
 function highlightActiveLink(){
     links = document.querySelectorAll('.nav-link')
@@ -259,7 +354,63 @@ function hideSpinner(){
 
 }
 
+// Display Slider Movies
+async function displaySlider() {
+  const { results } = await fetchData('movie/now_playing');
 
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+      </h4>
+    `;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
+// showAlert
+function showAlert(message, className){
+  // create alert div element
+  div = document.createElement('div')
+  div.classList.add('alert', className)
+  div.appendChild(document.createTextNode(message))
+  document.querySelector('#alert').appendChild(div)
+
+  setTimeout(() => div.remove(), 3000)
+}
 
 // follow page 
 function init(){
@@ -267,6 +418,7 @@ function init(){
         case '/flixx-app-theme/index.html':
         case '/flixx-app-theme/':
             console.log('home')
+            displaySlider();
             displayPopularMovies();             
             break;
         case '/flixx-app-theme/shows.html':
@@ -282,7 +434,8 @@ function init(){
             console.log('tv-details');
             break;
         case '/flixx-app-theme/search.html':
-        console.log('search');
+            search()
+            console.log('search');
             break; 
 
     }
